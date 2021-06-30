08586f10aa6fee6494c3cbb3c0d1d517d154e527
Elaine Santos <elaine.fullstackdeveloper@gmail.com>
2021-06-30 12:34:40 -0300

    Update Compras no Supermercado.js

--- Solucoes Aritmeticas em JavaScript/Compras no Supermercado.js
+++ Solucoes Aritmeticas em JavaScript/Compras no Supermercado.js
@@ -20,3 +20,19 @@ Saída: A saída contém N linhas, cada uma representando uma lista de compra, s
 -------------------------------------------------------------
 */
 
+let num = parseInt(gets());
+let index = 0;
+
+let listas = Array(num);
+let listaSet = Array(num);
+
+while (index < num ) {
+    listas[index] = gets().split(" ");
+    index++;
+}
+
+listas.map((i,d) => listaSet[d] = new Set(i));
+listaSet.map((i,d) => listas[d] = [...i]);
+
+listas.forEach(i => i.sort());
+listas.forEach(i => console.log(i.join(" ")));
