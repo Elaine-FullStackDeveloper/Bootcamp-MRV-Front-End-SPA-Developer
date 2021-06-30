51274df15b6decac912343dcd9db39deb35d74b3
Elaine Santos <elaine.fullstackdeveloper@gmail.com>
2021-06-30 13:52:01 -0300

    Update Area Superior.js

--- Desafios Intermediários em JavaScript/Area Superior.js
+++ Desafios Intermediários em JavaScript/Area Superior.js
@@ -1,4 +1,4 @@
-/*
+/* URI PROBLEMA 1187
 Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. Em seguida, calcule e mostre a soma ou a média 
 considerando somente aqueles elementos que estão na área superior da matriz, conforme ilustrado abaixo (área verde).
 
